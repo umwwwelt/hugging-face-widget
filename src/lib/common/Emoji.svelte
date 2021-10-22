354cbdebeb03bb10ebd51f8aabf907d1b8bd53eb
7antra <yann@boug.art>
2021-10-22 16:10:08 +0200

    feat: EmojiLoader + Emoji 🧷

---
+++ src/lib/common/Emoji.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+	//svelte
+	import { onMount } from 'svelte';
+	import { tweened } from 'svelte/motion';
+	import { elasticOut } from 'svelte/easing';
+
+	//type
+	import type { Coordonates } from '$lib/types';
+
+	//states
+	export let width: number = 30;
+	export let top: number = 0;
+	export let left: number = 0;
+	export let emoji: string = '🙂';
+	export let center: Coordonates;
+	export let i: number = 0; //index for delay animation
+
+	//animation
+	const opt = { duration: 1000, delay: 120 * i, easing: elasticOut };
+	const animTop = tweened(center.y - width / 2, opt);
+	const animLeft = tweened(center.x - width / 2, opt);
+	const animWidth = tweened(30, opt);
+
+	onMount(() => {
+		animTop.set(top || center.y - width / 2);
+		animLeft.set(left || center.x - width / 2);
+		animWidth.set(width);
+	});
+</script>
+
+<div
+	class="absolute {$$props.class}"
+	style="width:{width}px; top:{$animTop}px; left:{$animLeft}px;"
+>
+	<svg viewBox="0 0 20 20">
+		<text x="45%" y="80%" text-anchor="middle">{emoji}</text>
+	</svg>
+</div>
---
+++ src/lib/common/EmojiLoader.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+	import { onDestroy, onMount } from 'svelte';
+
+	import Emoji from './Emoji.svelte';
+
+	import type { Coordonates } from '$lib/types';
+
+	export let center: Coordonates;
+
+	let n: number = 0;
+	let iterations: NodeJS.Timeout;
+
+	const emojiList = [
+		'🤗',
+		'🙂',
+		'😀',
+		'😄',
+		'😅',
+		'😆',
+		'😝',
+		'😜',
+		'😛',
+		'😶',
+		'😐',
+		'😑',
+		'😒',
+		'😠',
+		'😞',
+		'😳',
+		'😢',
+		'🤗'
+	];
+
+	onMount(() => {
+		iterations = setInterval(() => (n >= emojiList.length - 1 ? (n = 0) : n++), 100);
+	});
+
+	onDestroy(() => {
+		clearInterval(iterations);
+	});
+</script>
+
+<Emoji width={30} {center} emoji={emojiList[n]} class={$$props.class} />
