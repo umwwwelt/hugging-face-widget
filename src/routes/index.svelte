1d91bc380608273940c826843fc6f37488b54b08
7antra <yann@boug.art>
2021-10-22 18:45:28 +0200

    clean: index 👍

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,6 +1,6 @@
 <script>
 	import Seo from '$lib/common/Meta/SEO.svelte';
-	import FaceToEmojiWidget from '$lib/widget/FaceToEmoji/FaceToEmojiWidget.svelte';
+	import FaceToEmojiWidget from '$lib/widgets/FaceToEmoji/FaceToEmojiWidget.svelte';
 	import { onMount } from 'svelte';
 
 	let loaded = false;
